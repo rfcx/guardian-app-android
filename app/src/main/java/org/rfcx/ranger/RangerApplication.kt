fd5c9164c6f715d50c3e02a4c483ad5a929a8c24
Komkrit  Banglad <jingjoeh@gmail.com>
2018-12-05 18:01:21 +0700

    Init realm config.

--- app/src/main/java/org/rfcx/ranger/RangerApplication.kt
+++ app/src/main/java/org/rfcx/ranger/RangerApplication.kt
@@ -7,24 +7,39 @@ import io.realm.RealmConfiguration
 import org.rfcx.ranger.service.ReportCleanupWorker
 
 class RangerApplication : Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-
-        Realm.init(this)
-        val config = RealmConfiguration.Builder()
-                .deleteRealmIfMigrationNeeded()
-                .build()
-        Realm.setDefaultConfiguration(config)
-
-        ReportCleanupWorker.enqueuePeriodically()
-
-        if (BuildConfig.USE_STETHO) {
-            Stetho.initialize(Stetho.newInitializerBuilder(this)
-                    .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
-                    .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
-                    .build())
-        }
-    }
-
+	
+	override fun onCreate() {
+		super.onCreate()
+		
+		Realm.init(this)
+		Realm.setDefaultConfiguration(defaultReamConfig())
+		
+		if (BuildConfig.VERSION_CODE == 12) {
+			// Remove old realm on next Release.
+			// TODO remove this when release a news version more than 12
+			val oldRealm = RealmConfiguration.Builder()
+					.name("default.realm")
+					.deleteRealmIfMigrationNeeded()
+					.build()
+			Realm.deleteRealm(oldRealm)
+		}
+		
+		ReportCleanupWorker.enqueuePeriodically()
+		
+		if (BuildConfig.USE_STETHO) {
+			Stetho.initialize(Stetho.newInitializerBuilder(this)
+					.enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+					.enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
+					.build())
+		}
+	}
+	
+	private fun defaultReamConfig(): RealmConfiguration {
+		return RealmConfiguration.Builder()
+				.name("Ranger.Realm")
+				.schemaVersion(1)
+				.deleteRealmIfMigrationNeeded()
+				.build()
+	}
+	
 }
