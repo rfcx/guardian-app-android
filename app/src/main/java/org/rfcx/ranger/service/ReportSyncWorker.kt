57663063a70289f8d20b8050c6a0de6cd77ba2dc
Komkrit  Banglad <jingjoeh@gmail.com>
2019-03-21 00:45:39 +0700

    For developing comment code for upload report , TODO fix later.

--- app/src/main/java/org/rfcx/ranger/service/ReportSyncWorker.kt
+++ app/src/main/java/org/rfcx/ranger/service/ReportSyncWorker.kt
@@ -4,10 +4,7 @@ import android.content.Context
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.work.*
-import org.rfcx.ranger.entity.Err
-import org.rfcx.ranger.entity.Ok
 import org.rfcx.ranger.localdb.ReportDb
-import org.rfcx.ranger.repo.api.SendReportApi
 import java.io.File
 
 
@@ -16,18 +13,18 @@ import java.io.File
  */
 
 class ReportSyncWorker(context: Context, params: WorkerParameters)
-    : Worker(context, params) {
+	: Worker(context, params) {
+	
+	override fun doWork(): Result {
+		/* Log.d(TAG, "doWork")
 
-    override fun doWork(): Result {
-        Log.d(TAG, "doWork")
+		 val api = SendReportApi()
+		 val db = ReportDb()
+		 val reports = db.lockUnsent()
 
-        val api = SendReportApi()
-        val db = ReportDb()
-        val reports = db.lockUnsent()
+		 Log.d(TAG, "doWork: found ${reports.size} unsent")
 
-        Log.d(TAG, "doWork: found ${reports.size} unsent")
-
-        var someFailed = false
+		 var someFailed = false
 
         for (report in reports) {
             Log.d(TAG, "doWork: sending ${report.id}")
@@ -48,35 +45,36 @@ class ReportSyncWorker(context: Context, params: WorkerParameters)
 
 //        deleteSentReports()
 
-        return if (someFailed) Result.retry() else Result.success()
-    }
-
-    private fun deleteSentReports() {
-        val db = ReportDb()
-        val leftoverFiles = db.deleteSent()
-
-        for (filename in leftoverFiles) {
-            Log.d(TAG, "deleteSentReports: $filename")
-            val file = File(filename)
-            if (file.exists()) {
-                val result = file.delete()
-                Log.d(TAG, "deleteSentReports success: $result")
-            }
-        }
-    }
-
-    companion object {
-        private const val TAG = "ReportSyncWorker"
-        private const val UNIQUE_WORK_KEY = "ReportSyncWorkerUniqueKey"
-
-        fun enqueue() {
-            val constraints = Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
-            val workRequest = OneTimeWorkRequestBuilder<ReportSyncWorker>().setConstraints(constraints).build()
-            WorkManager.getInstance().enqueueUniqueWork(UNIQUE_WORK_KEY, ExistingWorkPolicy.REPLACE, workRequest)
-        }
-
-        fun workInfos(): LiveData<List<WorkInfo>> {
-            return WorkManager.getInstance().getWorkInfosForUniqueWorkLiveData(UNIQUE_WORK_KEY)
-        }
-    }
+		 return if (someFailed) Result.retry() else Result.success()*/
+		return Result.success()
+	}
+	
+	private fun deleteSentReports() {
+		val db = ReportDb()
+		val leftoverFiles = db.deleteSent()
+		
+		for (filename in leftoverFiles) {
+			Log.d(TAG, "deleteSentReports: $filename")
+			val file = File(filename)
+			if (file.exists()) {
+				val result = file.delete()
+				Log.d(TAG, "deleteSentReports success: $result")
+			}
+		}
+	}
+	
+	companion object {
+		private const val TAG = "ReportSyncWorker"
+		private const val UNIQUE_WORK_KEY = "ReportSyncWorkerUniqueKey"
+		
+		fun enqueue() {
+			val constraints = Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
+			val workRequest = OneTimeWorkRequestBuilder<ReportSyncWorker>().setConstraints(constraints).build()
+			WorkManager.getInstance().enqueueUniqueWork(UNIQUE_WORK_KEY, ExistingWorkPolicy.REPLACE, workRequest)
+		}
+		
+		fun workInfos(): LiveData<List<WorkInfo>> {
+			return WorkManager.getInstance().getWorkInfosForUniqueWorkLiveData(UNIQUE_WORK_KEY)
+		}
+	}
 }
