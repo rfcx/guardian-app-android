10c3e7decb8a866d75a3d3effb40c7508d755212
Komkrit  Banglad <jingjoeh@gmail.com>
2019-04-30 00:02:37 +0700

    Save guid to ReportImage after sent report.

--- app/src/main/java/org/rfcx/ranger/entity/report/ReportImage.kt
+++ app/src/main/java/org/rfcx/ranger/entity/report/ReportImage.kt
@@ -2,16 +2,20 @@ package org.rfcx.ranger.entity.report
 
 import com.google.gson.annotations.Expose
 import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
 
 open class ReportImage(
-        @Expose(serialize = false)
-        var reportId: Int = 0,
-        @Expose(serialize = false)
-        var imageUrl: String? = null,
-        @Expose(serialize = false)
-        var syncState: Int = 0) : RealmObject() {
-
-        companion object {
-                const val FIELD_REPORT_ID = "reportId"
-        }
+		@PrimaryKey
+		var id: Int = 0,
+		@Expose(serialize = false)
+		var reportId: Int = 0,
+		var guid: String? = null,
+		@Expose(serialize = false)
+		var imageUrl: String? = null,
+		@Expose(serialize = false)
+		var syncState: Int = 0) : RealmObject() {
+	
+	companion object {
+		const val FIELD_REPORT_ID = "reportId"
+	}
 }
--- app/src/main/java/org/rfcx/ranger/localdb/ReportDb.kt
+++ app/src/main/java/org/rfcx/ranger/localdb/ReportDb.kt
@@ -10,90 +10,108 @@ import org.rfcx.ranger.entity.report.ReportImage
  */
 
 class ReportDb(val realm: Realm = Realm.getDefaultInstance()) {
-
-    fun unsentCount(): Long {
-        return realm.where(Report::class.java).notEqualTo("syncState", SENT).count()
-    }
-
-    fun save(report: Report, attachImages: List<String>? = null) {
-        realm.executeTransaction {
-            if (report.id == 0) {
-                report.id = (it.where(Report::class.java).max("id")?.toInt() ?: 0) + 1
-            }
-            it.insertOrUpdate(report)
-
-            // save attached image to be Report Image
-            attachImages?.forEach { attachImage ->
-                val reportImage = ReportImage(reportId = report.id, imageUrl = attachImage)
-                it.insertOrUpdate(reportImage)
-            }
-        }
-    }
-
-    fun lockUnsent(): List<Report> {
-        var unsentCopied: List<Report> = listOf()
-        realm.executeTransaction {
-            val unsent = it.where(Report::class.java).equalTo("syncState", UNSENT).findAll().createSnapshot()
-            unsentCopied = unsent.toList()
-            unsent.forEach {
-                it.syncState = SENDING
-            }
-        }
-        return unsentCopied
-    }
-
-    fun unlockSending() {
-        realm.executeTransaction {
-            val snapshot = it.where(Report::class.java).equalTo("syncState", SENDING).findAll().createSnapshot()
-            snapshot.forEach {
-                it.syncState = UNSENT
-            }
-        }
-    }
-
-    fun markUnsent(id: Int) {
-        mark(id = id, syncState = UNSENT)
-    }
-
-    fun markSent(id: Int, guid: String) {
-        mark(id, guid, SENT)
-    }
-
-    private fun mark(id: Int, guid:String? = null, syncState: Int) {
-        realm.executeTransaction {
-            val report = it.where(Report::class.java).equalTo("id", id).findFirst()
-            if (report != null) {
-                report.guid = guid
-                report.syncState = syncState
-            }
-        }
-    }
-
-    fun getReport(guid: String) : Report?{
-        return realm.where(Report::class.java).equalTo(Report.FIELD_GUID, guid).findFirst()
-    }
-
-    fun getReport(id: Int) : Report?{
-        return realm.where(Report::class.java).equalTo(Report.FIELD_ID, id).findFirst()
-    }
-
-    fun getReportImages(reportId: Int): List<ReportImage>? {
-        return realm.where(ReportImage::class.java).equalTo(ReportImage.FIELD_REPORT_ID, reportId).findAllAsync()
-    }
-
-    // Deletes sent reports, returns a list of files that can also be deleted
-    fun deleteSent(): List<String> {
-        val reports = realm.where(Report::class.java).equalTo("syncState", SENT).findAll()
-        val filenames = reports.mapNotNull { it.audioLocation }
-        realm.executeTransaction {
-            reports.deleteAllFromRealm()
-        }
-        return filenames
-    }
-
-    companion object {
-        private const val UNSENT = 0
-        private const val SENDING = 1
-        private const val SENT = 2
-    }
+	fun unsentCount(): Long {
+		return realm.where(Report::class.java).notEqualTo("syncState", SENT).count()
+	}
+	
+	fun save(report: Report, attachImages: List<String>? = null) {
+		realm.executeTransaction {
+			if (report.id == 0) {
+				report.id = (it.where(Report::class.java).max("id")?.toInt() ?: 0) + 1
+			}
+			it.insertOrUpdate(report)
+			
+			// save attached image to be Report Image
+			attachImages?.forEach { attachImage ->
+				val imageId = (it.where(ReportImage::class.java).max("id")?.toInt() ?: 0) + 1
+				val reportImage = ReportImage(imageId, reportId = report.id, imageUrl = attachImage)
+				it.insertOrUpdate(reportImage)
+			}
+		}
+	}
+	
+	fun lockUnsent(): List<Report> {
+		var unsentCopied: List<Report> = listOf()
+		realm.executeTransaction {
+			val unsent = it.where(Report::class.java).equalTo("syncState", UNSENT).findAll().createSnapshot()
+			unsentCopied = unsent.toList()
+			unsent.forEach {
+				it.syncState = SENDING
+			}
+		}
+		return unsentCopied
+	}
+	
+	fun unlockSending() {
+		realm.executeTransaction {
+			val snapshot = it.where(Report::class.java).equalTo("syncState", SENDING).findAll().createSnapshot()
+			snapshot.forEach {
+				it.syncState = UNSENT
+			}
+		}
+	}
+	
+	fun markUnsent(id: Int) {
+		mark(id = id, syncState = UNSENT)
+	}
+	
+	fun markSent(id: Int, guid: String) {
+		mark(id, guid, SENT)
+		saveGuIDtoImages(guid, id)
+	}
+	
+	private fun mark(id: Int, guid: String? = null, syncState: Int) {
+		realm.executeTransaction {
+			val report = it.where(Report::class.java).equalTo("id", id).findFirst()
+			if (report != null) {
+				report.guid = guid
+				report.syncState = syncState
+			}
+		}
+	}
+	
+	fun getReport(guid: String): Report? {
+		return realm.where(Report::class.java).equalTo(Report.FIELD_GUID, guid).findFirst()
+	}
+	
+	fun getReport(id: Int): Report? {
+		return realm.where(Report::class.java).equalTo(Report.FIELD_ID, id).findFirst()
+	}
+	
+	fun getReportImages(reportId: Int): List<ReportImage>? {
+		return realm.where(ReportImage::class.java).equalTo(ReportImage.FIELD_REPORT_ID, reportId).findAllAsync()
+	}
+	
+	// Deletes sent reports, returns a list of files that can also be deleted
+	fun deleteSent(): List<String> {
+		val reports = realm.where(Report::class.java).equalTo("syncState", SENT).findAll()
+		val filenames = reports.mapNotNull { it.audioLocation }
+		realm.executeTransaction {
+			reports.deleteAllFromRealm()
+		}
+		return filenames
+	}
+	
+	fun getAllAsync(): List<Report> {
+		return realm.copyFromRealm(realm.where(Report::class.java).findAllAsync())
+	}
+	
+	private fun saveGuIDtoImages(guid: String, reportId: Int) {
+		val images = realm.where(ReportImage::class.java).equalTo("reportId", reportId).findAll()
+		Log.d("saveGuIDtoImages", "${images.count()}")
+		realm.executeTransaction { transition ->
+			images?.forEach {
+				val image = it.apply {
+					this.guid = guid
+				}
+				transition.insertOrUpdate(image)
+			}
+		}
+	}
+	
+	companion object {
+		const val UNSENT = 0
+		const val SENDING = 1
+		const val SENT = 2
+	}
 }
