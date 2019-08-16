f52564930f58cd21c8bb23c8560296eb46a406ff
ratree <ratree.ochn@gmail.com>
2019-08-16 11:09:04 +0700

    CA-1351 Invite code - Implement invite code screen (API)

--- app/src/main/java/org/rfcx/ranger/view/login/InvitationCodeFragment.kt
+++ app/src/main/java/org/rfcx/ranger/view/login/InvitationCodeFragment.kt
@@ -1,5 +1,6 @@
 package org.rfcx.ranger.view.login
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,22 +9,27 @@ import kotlinx.android.synthetic.main.fragment_invitation_code.*
 import org.rfcx.ranger.R
 import org.rfcx.ranger.view.base.BaseFragment
 
-class InvitationCodeFragment: BaseFragment() {
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_invitation_code, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        initView()
-    }
-
-    private fun initView() {
-        submitButton.setOnClickListener {
-            val code = inputCodeEditText.text.toString()
-
-            // TODO doSubmit
-        }
-    }
+class InvitationCodeFragment : BaseFragment() {
+	lateinit var listener: LoginListener
+	
+	override fun onAttach(context: Context) {
+		super.onAttach(context)
+		listener = (context as LoginListener)
+	}
+	
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+		return inflater.inflate(R.layout.fragment_invitation_code, container, false)
+	}
+	
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		initView()
+	}
+	
+	private fun initView() {
+		submitButton.setOnClickListener {
+			val code = inputCodeEditText.text.toString()
+			listener.doSubmit(code)
+		}
+	}
 }
--- app/src/main/java/org/rfcx/ranger/view/login/LoginActivityNew.kt
+++ app/src/main/java/org/rfcx/ranger/view/login/LoginActivityNew.kt
@@ -1,14 +1,17 @@
 package org.rfcx.ranger.view.login
 
+import android.app.Dialog
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.auth0.android.Auth0
 import com.auth0.android.authentication.AuthenticationAPIClient
 import com.auth0.android.authentication.AuthenticationException
 import com.auth0.android.callback.BaseCallback
+import com.auth0.android.provider.AuthCallback
 import com.auth0.android.provider.WebAuthProvider
 import com.auth0.android.result.Credentials
 import com.crashlytics.android.Crashlytics
@@ -19,124 +22,207 @@ import org.rfcx.ranger.R
 import org.rfcx.ranger.entity.Err
 import org.rfcx.ranger.entity.Ok
 import org.rfcx.ranger.entity.user.UserAuthResponse
+import org.rfcx.ranger.repo.api.InvitationCodeApi
 import org.rfcx.ranger.repo.api.UserTouchApi
 import org.rfcx.ranger.util.CredentialKeeper
 import org.rfcx.ranger.util.CredentialVerifier
-import org.rfcx.ranger.view.InvitationActivity
+import org.rfcx.ranger.util.Preferences
 import org.rfcx.ranger.view.MainActivityNew
 
 
 // TODO change class name
 class LoginActivityNew : AppCompatActivity(), LoginListener {
-
-    companion object {
-        fun startActivity(context: Context) {
-            val intent = Intent(context, LoginActivityNew::class.java)
-            context.startActivity(intent)
-        }
-    }
-
-    private val auth0 by lazy {
-        val auth0 = Auth0(getString(R.string.auth0_client_id), getString(R.string.auth0_domain))
-        //auth0.isLoggingEnabled = true
-        auth0.isOIDCConformant = true
-        auth0
-    }
-
-    private val authentication by lazy {
-        AuthenticationAPIClient(auth0)
-    }
-
-    private val webAuthentication by lazy {
-        WebAuthProvider.init(auth0)
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_new)
-
-        // TODO hasValidCredentials()
-
-        supportFragmentManager.beginTransaction()
-                .replace(loginContainer.id, LoginFragment(),
-                        "LoginFragment").commit()
-    }
-
-    override fun onLoginWithFacebook() {
-
-        // TODO doFacebookLogin()
-
-        supportFragmentManager.beginTransaction()
-                .replace(loginContainer.id, InvitationCodeFragment(),
-                        "InvitationCodeFragment").commit()
-    }
-
-    override fun doLogin(email: String, password: String) {
-        loginErrorTextView.visibility = View.INVISIBLE
-
-        authentication
-                .login(email, password, "Username-Password-Authentication")
-                .setScope(getString(R.string.auth0_scopes))
-                .setAudience(getString(R.string.auth0_audience))
-                .start(object : BaseCallback<Credentials, AuthenticationException> {
-                    override fun onSuccess(credentials: Credentials) {
-                        val result = CredentialVerifier(this@LoginActivityNew).verify(credentials)
-                        when (result) {
-                            is Err -> {
-                                loginFailed(result.error)
-                            }
-                            is Ok -> {
-                                loginSuccess(result.value)
-                            }
-                        }
-                    }
-
-                    override fun onFailure(exception: AuthenticationException) {
-                        exception.printStackTrace()
-                        Crashlytics.logException(exception)
-                        if (exception.code == "invalid_grant") {
-                            loginFailed(getString(R.string.incorrect_username_password))
-                        } else {
-                            loginFailed(exception.description)
-                        }
-                    }
-                })
-    }
-
-    private fun loginFailed(errorMessage: String?) {
-        runOnUiThread {
-            loginErrorTextView.text = errorMessage
-            loginErrorTextView.visibility = View.VISIBLE
-        }
-    }
-
-    private fun loginSuccess(userAuthResponse: UserAuthResponse) {
-
-        CredentialKeeper(this@LoginActivityNew).save(userAuthResponse)
-
-        UserTouchApi().send(this, object : UserTouchApi.UserTouchCallback {
-            override fun onSuccess() {
-                if (userAuthResponse.isRanger) {
-                    MainActivityNew.startActivity(this@LoginActivityNew)
-                } else {
-                    this@LoginActivityNew.startActivity(Intent(this@LoginActivityNew, InvitationActivity::class.java))
-                }
-                finish()
-            }
-
-            override fun onFailed(t: Throwable?, message: String?) {
-                runOnUiThread {
-                    loginProgress.visibility = View.INVISIBLE
-                    loginGroupView.visibility = View.VISIBLE
-                }
-                Crashlytics.logException(t)
-                loginFailed(message ?: t?.localizedMessage)
-            }
-        })
-    }
+	
+	companion object {
+		fun startActivity(context: Context) {
+			val intent = Intent(context, LoginActivityNew::class.java)
+			context.startActivity(intent)
+		}
+	}
+	
+	private val auth0 by lazy {
+		val auth0 = Auth0(getString(R.string.auth0_client_id), getString(R.string.auth0_domain))
+		//auth0.isLoggingEnabled = true
+		auth0.isOIDCConformant = true
+		auth0
+	}
+	
+	private val authentication by lazy {
+		AuthenticationAPIClient(auth0)
+	}
+	
+	private val webAuthentication by lazy {
+		WebAuthProvider.init(auth0)
+	}
+	
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_login_new)
+		
+		// TODO hasValidCredentials()
+		
+		supportFragmentManager.beginTransaction()
+				.add(loginContainer.id, LoginFragment(),
+						"LoginFragment").commit()
+	}
+	
+	override fun onLoginWithFacebook() {
+		loginErrorTextView.visibility = View.INVISIBLE
+		
+		webAuthentication
+				.withConnection("facebook")
+				.withScope(getString(R.string.auth0_scopes))
+				.withScheme(getString(R.string.auth0_scheme))
+				.withAudience(getString(R.string.auth0_audience))
+				.start(this@LoginActivityNew, object : AuthCallback {
+					override fun onFailure(dialog: Dialog) {
+						loginFailed(null)
+					}
+					
+					override fun onFailure(exception: AuthenticationException) {
+						Crashlytics.logException(exception)
+						loginFailed(exception.localizedMessage)
+					}
+					
+					override fun onSuccess(credentials: Credentials) {
+						val result = CredentialVerifier(this@LoginActivityNew).verify(credentials)
+						when (result) {
+							is Err -> {
+								loginFailed(result.error)
+							}
+							is Ok -> {
+								loginSuccess(result.value)
+							}
+						}
+					}
+				})
+	}
+	
+	override fun doLogin(email: String, password: String) {
+		loginErrorTextView.visibility = View.INVISIBLE
+		
+		authentication
+				.login(email, password, "Username-Password-Authentication")
+				.setScope(getString(R.string.auth0_scopes))
+				.setAudience(getString(R.string.auth0_audience))
+				.start(object : BaseCallback<Credentials, AuthenticationException> {
+					override fun onSuccess(credentials: Credentials) {
+						val result = CredentialVerifier(this@LoginActivityNew).verify(credentials)
+						when (result) {
+							is Err -> {
+								loginFailed(result.error)
+							}
+							is Ok -> {
+								loginSuccess(result.value)
+							}
+						}
+					}
+					
+					override fun onFailure(exception: AuthenticationException) {
+						exception.printStackTrace()
+						Crashlytics.logException(exception)
+						if (exception.code == "invalid_grant") {
+							loginFailed(getString(R.string.incorrect_username_password))
+						} else {
+							loginFailed(exception.description)
+						}
+					}
+				})
+	}
+	
+	private fun loginFailed(errorMessage: String?) {
+		runOnUiThread {
+			loginErrorTextView.text = errorMessage
+			loginErrorTextView.visibility = View.VISIBLE
+		}
+	}
+	
+	private fun loginSuccess(userAuthResponse: UserAuthResponse) {
+		
+		CredentialKeeper(this@LoginActivityNew).save(userAuthResponse)
+		
+		UserTouchApi().send(this, object : UserTouchApi.UserTouchCallback {
+			override fun onSuccess() {
+				if (userAuthResponse.isRanger) {
+					MainActivityNew.startActivity(this@LoginActivityNew)
+					finish()
+				} else {
+					supportFragmentManager.beginTransaction()
+							.replace(loginContainer.id, InvitationCodeFragment(),
+									"InvitationCodeFragment").commit()
+				}
+				
+			}
+			
+			override fun onFailed(t: Throwable?, message: String?) {
+				runOnUiThread {
+					loginProgress.visibility = View.INVISIBLE
+					loginGroupView.visibility = View.VISIBLE
+				}
+				Crashlytics.logException(t)
+				loginFailed(message ?: t?.localizedMessage)
+			}
+		})
+	}
+	
+	override fun doSubmit(code: String) {
+		submit(code) { success ->
+			if (success) {
+				MainActivityNew.startActivity(this@LoginActivityNew)
+				finish()
+			} else {
+				Toast.makeText(this@LoginActivityNew, "Problem", Toast.LENGTH_LONG).show() // TODO: handle error
+			}
+		}
+	}
+	
+	private fun submit(code: String, callback: (Boolean) -> Unit) {
+		InvitationCodeApi().send(this, code, object : InvitationCodeApi.InvitationCodeCallback {
+			override fun onSuccess() {
+				refreshToken { success ->
+					callback(success)
+				}
+			}
+			
+			override fun onFailed(t: Throwable?, message: String?) {
+				callback(false)
+			}
+		})
+	}
+	
+	private fun refreshToken(callback: (Boolean) -> Unit) {
+		val refreshToken = Preferences.getInstance(this).getString(Preferences.REFRESH_TOKEN)
+		if (refreshToken == null) {
+			callback(false)
+			return
+		}
+		
+		authentication.renewAuth(refreshToken).start(object : BaseCallback<Credentials, AuthenticationException> {
+			override fun onSuccess(credentials: Credentials) {
+				val result = CredentialVerifier(this@LoginActivityNew).verify(credentials)
+				when (result) {
+					is Err -> {
+						callback(false)
+					}
+					is Ok -> {
+						val userAuthResponse = result.value
+						if (userAuthResponse.isRanger) {
+							CredentialKeeper(this@LoginActivityNew).save(userAuthResponse)
+						}
+						callback(userAuthResponse.isRanger)
+					}
+				}
+			}
+			
+			override fun onFailure(error: AuthenticationException) {
+				callback(false)
+			}
+		})
+	}
 }
 
 interface LoginListener {
-    fun onLoginWithFacebook()
-    fun doLogin(email: String, password: String)
+	fun onLoginWithFacebook()
+	fun doLogin(email: String, password: String)
+	fun doSubmit(code: String)
 }
