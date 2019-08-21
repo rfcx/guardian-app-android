ccb85d06ae2d3add611c0095d2e6c9ad4f8d92f0
Komkrit  Banglad <jingjoeh@gmail.com>
2019-08-21 19:26:13 +0700

    Refactor class.

--- app/src/main/java/org/rfcx/ranger/util/EventExt.kt
+++ app/src/main/java/org/rfcx/ranger/util/EventExt.kt
@@ -8,36 +8,57 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 fun Event.getIconRes(): Int {
+	
+	return when (this.value) {
+		Event.chainsaw -> R.drawable.ic_chainsaw
+		Event.gunshot -> R.drawable.ic_gun
+		Event.vehicle -> R.drawable.ic_truck
+		Event.trespasser -> R.drawable.ic_people
+		else -> R.drawable.ic_other
+	}
+}
 
-    return when (this.value) {
-        Event.chainsaw -> R.drawable.ic_chainsaw
-        Event.gunshot -> R.drawable.ic_gun
-        Event.vehicle -> R.drawable.ic_truck
-        Event.trespasser -> R.drawable.ic_people
-        else -> R.drawable.ic_other
-    }
+fun String.toEventPosition(): Int {
+	return when (this) {
+		Event.chainsaw -> 2
+		Event.gunshot -> 3
+		Event.vehicle -> 0
+		Event.trespasser -> 1
+		Event.other -> 4
+		else -> -1
+	}
+}
+
+fun String.toEventIcon(): Int {
+	return when (this) {
+		Event.chainsaw -> R.drawable.ic_chainsaw
+		Event.gunshot -> R.drawable.ic_gun
+		Event.vehicle -> R.drawable.ic_truck
+		Event.trespasser -> R.drawable.ic_people
+		else -> R.drawable.ic_other
+	}
 }
 
 fun Event.timeAgoDisplay(context: Context): String {
-    beginsAt ?: return ""
-
-    val eventDate = DateHelper.getDateTime(beginsAt)
-    // timestamp
-    val dayAgo = DAY
-    val daysAgo = 2 * DAY
-
-    val diff = Date().time - eventDate!!.time
-    val diffCalendar = Calendar.getInstance()
-    diffCalendar.timeInMillis = diff
-
-    val timeFormat = SimpleDateFormat(DateHelper.timeFormat, Locale.US)
-
-    return if (diff < dayAgo) {
-        timeFormat.format(eventDate.time)
-    } else if (diff < daysAgo) {
-        "${context.getString(R.string.yesterday)} ${timeFormat.format(eventDate.time)}"
-    } else {
-        val dateFormat = SimpleDateFormat("MMMM d, yyyy HH:mm", Locale.US)
-        dateFormat.format(eventDate.time)
-    }
+	beginsAt ?: return ""
+	
+	val eventDate = DateHelper.getDateTime(beginsAt)
+	// timestamp
+	val dayAgo = DAY
+	val daysAgo = 2 * DAY
+	
+	val diff = Date().time - eventDate!!.time
+	val diffCalendar = Calendar.getInstance()
+	diffCalendar.timeInMillis = diff
+	
+	val timeFormat = SimpleDateFormat(DateHelper.timeFormat, Locale.US)
+	
+	return if (diff < dayAgo) {
+		timeFormat.format(eventDate.time)
+	} else if (diff < daysAgo) {
+		"${context.getString(R.string.yesterday)} ${timeFormat.format(eventDate.time)}"
+	} else {
+		val dateFormat = SimpleDateFormat("MMMM d, yyyy HH:mm", Locale.US)
+		dateFormat.format(eventDate.time)
+	}
 }
