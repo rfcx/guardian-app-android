956abdb4e6d636867b3a1575528eead063323abe
Komkrit  Banglad <jingjoeh@gmail.com>
2019-09-04 21:46:30 +0700

    CA-1425 Map - normalise the checkin points to improve performance. # Not done yet **need to change MAXIMUM_CHECK_IN_FOR_DISPLAY.

--- app/src/main/java/org/rfcx/ranger/view/map/MapViewModel.kt
+++ app/src/main/java/org/rfcx/ranger/view/map/MapViewModel.kt
@@ -3,7 +3,6 @@ package org.rfcx.ranger.view.map
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
-import io.realm.RealmResults
 import org.rfcx.ranger.entity.location.CheckIn
 import org.rfcx.ranger.entity.report.Report
 import org.rfcx.ranger.localdb.LocationDb
@@ -13,7 +12,7 @@ import org.rfcx.ranger.util.asLiveData
 class MapViewModel(private val reportDb: ReportDb, private val locationDb: LocationDb) : ViewModel() {
 	
 	fun getReports(): LiveData<List<Report>> {
-		return Transformations.map<RealmResults<Report>, List<Report>>(
+		return Transformations.map(
 				reportDb.getAllResultsAsync().asLiveData()
 		) {
 			it
@@ -21,10 +20,28 @@ class MapViewModel(private val reportDb: ReportDb, private val locationDb: Locat
 	}
 	
 	fun getCheckIns(): LiveData<List<CheckIn>> {
-		return Transformations.map<RealmResults<CheckIn>, List<CheckIn>>(
+		return Transformations.map(
 				locationDb.allForDisplay().asLiveData()
 		) {
-			it
+			
+			if (it.count() <= MAXIMUM_CHECK_IN_FOR_DISPLAY) {
+				it
+			} else {
+				val displayPoints = arrayListOf<CheckIn>()
+				val interestModPosition = it.count() / (MAXIMUM_CHECK_IN_FOR_DISPLAY)
+				it.forEachIndexed { index, checkIn ->
+					if (index % interestModPosition == 0) {
+						displayPoints.add(checkIn)
+					} else if (index == it.count() - 1) {
+						displayPoints.add(checkIn)
+					}
+				}
+				displayPoints
+			}
 		}
+	}
+	
+	companion object {
+		private const val MAXIMUM_CHECK_IN_FOR_DISPLAY = 10
 	}
 }
