431300dcf682dbe86f6fd7f6338a256e63a3f013
Anuphap Suwannamas <anupharpae@gmail.com>
2019-09-15 12:35:24 +0700

    CA-1496 Alert - if there is no guardian group, show text and button to take the user to set a guardian group

--- app/src/main/java/org/rfcx/ranger/view/alerts/AlertViewModel.kt
+++ app/src/main/java/org/rfcx/ranger/view/alerts/AlertViewModel.kt
@@ -1,11 +1,22 @@
 package org.rfcx.ranger.view.alerts
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import org.rfcx.ranger.data.local.ProfileData
 import org.rfcx.ranger.entity.event.Event
 
-class AlertViewModel : ViewModel() {
+class AlertViewModel(private val profileData: ProfileData) : ViewModel() {
+	
+	var hasGuardianGroup = profileData.hasGuardianGroup()
 	
 	val eventFromNotification = MutableLiveData<Event>()
 	
+	private val _observeGuardianGroup = MutableLiveData<Boolean>()
+	val observeGuardianGroup: LiveData<Boolean> = _observeGuardianGroup
+	
+	fun resumed() {
+		hasGuardianGroup =  profileData.hasGuardianGroup()
+		_observeGuardianGroup.value = profileData.hasGuardianGroup()
+	}
 }
