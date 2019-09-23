dec6068c8290c0710d69b0088b27154a0bd834f3
ratree <ratree.ochn@gmail.com>
2019-09-23 16:28:42 +0700

    UnitTest - Update unit test  in reportDetailViewModelUnitTest

--- app/src/test/java/org/rfcx/ranger/ReportDetailViewModelUnitTest.kt
+++ app/src/test/java/org/rfcx/ranger/ReportDetailViewModelUnitTest.kt
@@ -1,16 +1,22 @@
 package org.rfcx.ranger
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
 
 import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
+import org.rfcx.ranger.localdb.ReportDb
 import org.rfcx.ranger.localdb.ReportImageDb
 import org.rfcx.ranger.view.report.ReportDetailViewModel
 
 
 
 class ReportDetailViewModelUnitTest {
+	@Rule
+	@JvmField
+	val rule = InstantTaskExecutorRule()
 	
 	@Test
 	fun `1 + 1 = 2`() {
@@ -19,24 +25,25 @@ class ReportDetailViewModelUnitTest {
 	
 	@Test
 	fun getReport() {
+		
 		val viewModel = ReportDetailViewModel(mock(), mock())
 		
-//		viewModel.setReport(1)
-//		viewModel.setReport(mock())
+		viewModel.setReport(1)
 		
 		viewModel.getReport()
 	}
 	
-	@Test
-	fun getReportImages(){
-		val reportImageDb = mock<ReportImageDb>()
-		val viewModel = ReportDetailViewModel(mock(), reportImageDb)
-		
-//		val reportImages = viewModel.reportImages
-		val result = viewModel.getReportImages()
-
-//		assertEquals(reportImages, result.value)
-	}
+//	@Test
+//	fun getReportImages(){
+//		val reportDb = mock<ReportDb>()
+//		val reportImageDb = mock<ReportImageDb>()
+//		val viewModel = ReportDetailViewModel(reportDb, reportImageDb)
+//
+//		viewModel.setReport(mock())
+////		val result = viewModel.getReportImages()
+//
+////		assertEquals(reportImages, result.value)
+//	}
 	
 //	@Test
 //	fun addReportImages(){
