07ab4003e4cf2878f2f7843e443f5bdc3fe4ab00
Komkrit  Banglad <jingjoeh@gmail.com>
2019-09-24 22:00:22 +0700

    Set bottom sheet anchor to BOTTOM before showing.

--- app/src/main/java/org/rfcx/ranger/view/MainActivityNew.kt
+++ app/src/main/java/org/rfcx/ranger/view/MainActivityNew.kt
@@ -3,8 +3,10 @@ package org.rfcx.ranger.view
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import androidx.appcompat.app.AlertDialog
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import com.google.android.material.bottomsheet.BottomSheetBehavior
@@ -22,6 +24,7 @@ import org.rfcx.ranger.view.profile.ProfileFragment
 import org.rfcx.ranger.view.report.ReportActivity
 import org.rfcx.ranger.view.status.StatusFragment
 import org.rfcx.ranger.widget.BottomNavigationMenuItem
+
 
 // TODO change class name
 class MainActivityNew : BaseActivity(), MainActivityEventListener {
@@ -154,6 +157,11 @@ class MainActivityNew : BaseActivity(), MainActivityEventListener {
 	
 	override fun showBottomSheet(fragment: Fragment) {
 		hidBottomAppBar()
+		val layoutParams: CoordinatorLayout.LayoutParams = bottomSheetContainer.layoutParams
+				as CoordinatorLayout.LayoutParams
+		layoutParams.anchorGravity = Gravity.BOTTOM
+		bottomSheetContainer.layoutParams = layoutParams
+		
 		supportFragmentManager.beginTransaction()
 				.replace(bottomSheetContainer.id, fragment, "BottomSheet")
 				.commit()
