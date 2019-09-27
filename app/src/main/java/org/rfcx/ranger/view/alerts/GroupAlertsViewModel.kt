007316e3c902d58dcc459cc95a6fdff37baed938
ratree <ratree.ochn@gmail.com>
2019-09-27 10:54:46 +0700

    GroupAlertsViewModel- Group Guardian event

--- app/src/main/java/org/rfcx/ranger/view/alerts/GroupAlertsViewModel.kt
+++ app/src/main/java/org/rfcx/ranger/view/alerts/GroupAlertsViewModel.kt
@@ -10,16 +10,22 @@ import org.rfcx.ranger.data.remote.Result
 import org.rfcx.ranger.data.remote.domain.BaseDisposableSingle
 import org.rfcx.ranger.data.remote.groupByGuardians.GroupByGuardiansUseCase
 import org.rfcx.ranger.data.remote.groupByGuardians.eventInGuardian.GetEventInGuardian
+import org.rfcx.ranger.entity.event.Event
 import org.rfcx.ranger.entity.event.EventResponse
 import org.rfcx.ranger.entity.event.EventsGuardianRequestFactory
 import org.rfcx.ranger.entity.guardian.GroupByGuardiansResponse
 import org.rfcx.ranger.entity.guardian.Guardian
 import org.rfcx.ranger.util.Preferences
+import java.util.*
+import kotlin.collections.ArrayList
 
 class GroupAlertsViewModel(private val context: Context, private val groupByGuardiansUseCase: GroupByGuardiansUseCase, private val getEventInGuardian: GetEventInGuardian) : ViewModel() {
 	
 	private val _items = MutableLiveData<Result<GroupByGuardiansResponse>>()
 	val items: LiveData<Result<GroupByGuardiansResponse>> get() = _items
+	
+	private val _groupsEventsGuardian = MutableLiveData<Result<ArrayList<MutableList<Event>>>>()
+	val groupsEventsGuardian: LiveData<Result<ArrayList<MutableList<Event>>>> get() = _groupsEventsGuardian
 	
 	init {
 		loadGuardianGroups()
@@ -41,7 +47,7 @@ class GroupAlertsViewModel(private val context: Context, private val groupByGuar
 		val requestFactory = EventsGuardianRequestFactory(groupGuid, "begins_at", "DESC", LIMITS, 0)
 		getEventInGuardian.execute(object : DisposableSingleObserver<EventResponse>() {
 			override fun onSuccess(t: EventResponse) {
-				Log.d("size", t.events?.size.toString())
+				t.events?.let { makeGroupGuardian(it) }
 			}
 			
 			override fun onError(e: Throwable) {
@@ -49,6 +55,29 @@ class GroupAlertsViewModel(private val context: Context, private val groupByGuar
 			}
 			
 		}, requestFactory)
+	}
+	
+	private fun makeGroupGuardian(list: List<Event>){
+		val name = Array(list.size){ i -> list[i].guardianShortname.toString()}
+		val listShortName = removeArrayDuplicates(name)
+		
+		val listEvents = ArrayList<Event>()
+		val allListEvents: ArrayList<MutableList<Event>> = ArrayList()
+		
+		listShortName.forEach { shortName ->
+			list.forEach { event ->
+				if(shortName == event.guardianShortname){
+					listEvents.add(event)
+				}
+			}
+			allListEvents.addAll(listOf(listEvents))
+			listEvents.clear()
+		}
+		Log.d("AllListEvents", allListEvents.size.toString())
+	}
+	
+	private fun removeArrayDuplicates(duplicates: Array<String>): Array<String> {
+		return Arrays.asList(*duplicates).toSet().toTypedArray()
 	}
 	
 	companion object {
