9d08afc659a20d08135744c5d1229d898038ce87
Anuphap Suwannamas <anupharpae@gmail.com>
2019-09-29 19:04:14 +0700

    CA-1541 Map - if airplane mode enabled, tell user to disable it before using map

--- app/src/main/java/org/rfcx/ranger/view/base/BaseFragment.kt
+++ app/src/main/java/org/rfcx/ranger/view/base/BaseFragment.kt
@@ -2,4 +2,9 @@ package org.rfcx.ranger.view.base
 
 import androidx.fragment.app.Fragment
 
-abstract class BaseFragment : Fragment()
+
+abstract class BaseFragment : Fragment() {
+	protected fun isSafe(): Boolean {
+		return !(this.isRemoving || this.activity == null || this.isDetached || !this.isAdded || this.view == null)
+	}
+}
