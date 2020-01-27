a374aeb33f26064d4a0ab047ece98ec37c02bee8
ratree <ratree.ochn@gmail.com>
2020-01-27 18:22:08 +0700

    CA-1992 Update UI for ChangePassword- 2

--- app/src/main/java/org/rfcx/ranger/view/profile/PasswordChangeActivity.kt
+++ app/src/main/java/org/rfcx/ranger/view/profile/PasswordChangeActivity.kt
@@ -6,14 +6,10 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.core.graphics.drawable.DrawableCompat
 import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.activity_password_change.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -21,7 +17,6 @@ import org.rfcx.ranger.R
 import org.rfcx.ranger.data.remote.success
 
 class PasswordChangeActivity : AppCompatActivity() {
-	private var menu: Menu? = null
 	private val passwordChangeViewModel: PasswordChangeViewModel by viewModel()
 	
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,42 +26,41 @@ class PasswordChangeActivity : AppCompatActivity() {
 		setupToolbar()
 		newPasswordEditText.showKeyboard()
 		
-		newPasswordEditText.addTextChangedListener(object : TextWatcher {
-			override fun afterTextChanged(p0: Editable?) {
-				if (p0 != null) {
-					if (p0.isNotEmpty() && p0.length < 6) {
-						errorNewPasswordTextView.text = getString(R.string.password_must_have_at_least_6_characters)
-						errorNewPasswordTextView.visibility = View.VISIBLE
-						DrawableCompat.setTint(newPasswordEditText.background, ContextCompat.getColor(this@PasswordChangeActivity, R.color.text_error))
-					}
-					
-					if (p0.isNotEmpty() && p0.length >= 6) {
-						errorNewPasswordTextView.visibility = View.GONE
-						DrawableCompat.setTint(newPasswordEditText.background, ContextCompat.getColor(this@PasswordChangeActivity, R.color.gray_30))
+		newPasswordEditText.setOnFocusChangeListener { _, hasFocus ->
+			if (!hasFocus) {
+				errorNewPasswordTextView.visibility = View.VISIBLE
+				when {
+					newPasswordEditText.text.isNullOrEmpty() -> errorNewPasswordTextView.text = getString(R.string.please_enter_your_new_password)
+					newPasswordEditText.text!!.length < 6 -> errorNewPasswordTextView.text = getString(R.string.password_must_have_at_least_6_characters)
+					newPasswordEditText.text!!.length >= 6 -> errorNewPasswordTextView.visibility = View.GONE
+				}
+			}
+		}
+		
+		newPasswordAgainEditText.setOnFocusChangeListener { _, hasFocus ->
+			if (!hasFocus) {
+				errorNewPasswordAgainTextView.visibility = View.VISIBLE
+				when {
+					newPasswordAgainEditText.text.isNullOrEmpty() -> errorNewPasswordAgainTextView.text = getString(R.string.please_confirm_your_new_password)
+					newPasswordAgainEditText.text!!.length < 6 -> errorNewPasswordAgainTextView.text = getString(R.string.password_must_have_at_least_6_characters)
+					newPasswordAgainEditText.text!!.length >= 6 -> {
+						errorNewPasswordAgainTextView.visibility = View.GONE
+						updatePasswordButton.isEnabled = true
 					}
 				}
 			}
-			
-			override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-				Log.d("addTextChangedListener", "beforeTextChanged")
-			}
-			
-			override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-				Log.d("addTextChangedListener","onTextChanged")
-			}
-		})
+		}
 		
 		newPasswordAgainEditText.addTextChangedListener(object : TextWatcher {
 			override fun afterTextChanged(p0: Editable?) {
 				if (p0 != null) {
-					if (p0.isNotEmpty() && newPasswordEditText.text !== p0) {
-						errorNewPasswordAgainTextView.text = getString(R.string.confirm_password_does_not_match)
-						errorNewPasswordAgainTextView.visibility = View.VISIBLE
-						DrawableCompat.setTint(newPasswordAgainEditText.background, ContextCompat.getColor(this@PasswordChangeActivity, R.color.text_error))
-					}
-					if (newPasswordEditText.text.toString() == newPasswordAgainEditText.text.toString()) {
-						errorNewPasswordAgainTextView.visibility = View.GONE
-						DrawableCompat.setTint(newPasswordAgainEditText.background, ContextCompat.getColor(this@PasswordChangeActivity, R.color.gray_30))
+					if (!newPasswordAgainEditText.text.isNullOrEmpty()) {
+						if (newPasswordAgainEditText.text!!.length >= 6) {
+							updatePasswordButton.isEnabled = true
+							errorNewPasswordAgainTextView.visibility = View.GONE
+						} else {
+							updatePasswordButton.isEnabled = false
+						}
 					}
 				}
 			}
@@ -76,9 +70,21 @@ class PasswordChangeActivity : AppCompatActivity() {
 			}
 			
 			override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-				Log.d("addTextChangedListener","onTextChanged")
+				Log.d("addTextChangedListener", "onTextChanged")
 			}
 		})
+		
+		updatePasswordButton.setOnClickListener {
+			if (newPasswordEditText.text.toString() == newPasswordAgainEditText.text.toString()) {
+				errorNewPasswordAgainTextView.visibility = View.GONE
+				passwordChangeViewModel.changeUserPassword(newPasswordEditText.text.toString())
+				updatePasswordButton.hideKeyboard()
+			} else {
+				errorNewPasswordAgainTextView.visibility = View.VISIBLE
+				errorNewPasswordAgainTextView.text = getString(R.string.confirm_password_does_not_match)
+				updatePasswordButton.isEnabled = false
+			}
+		}
 		
 		passwordChangeViewModel.status.observe(this, Observer { it ->
 			it.success({
@@ -95,60 +101,6 @@ class PasswordChangeActivity : AppCompatActivity() {
 				loadingProgress.visibility = View.VISIBLE
 			})
 		})
-	}
-	
-	override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-		this.menu = menu
-		val inflater = menuInflater
-		inflater.inflate(R.menu.password_change, menu)
-		return super.onCreateOptionsMenu(menu)
-	}
-	
-	override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-		when (item?.itemId) {
-			android.R.id.home -> finish()
-			R.id.passwordChangeView -> passwordChange()
-		}
-		return super.onOptionsItemSelected(item)
-	}
-	
-	private fun passwordChange() {
-		val sendFeedbackView = findViewById<View>(R.id.passwordChangeView)
-		
-		errorNewPasswordTextView.visibility = View.GONE
-		DrawableCompat.setTint(newPasswordEditText.background, ContextCompat.getColor(this, R.color.gray_30))
-		
-		errorNewPasswordAgainTextView.visibility = View.GONE
-		DrawableCompat.setTint(newPasswordAgainEditText.background, ContextCompat.getColor(this, R.color.gray_30))
-		
-		if (newPasswordEditText.text.isNullOrEmpty()) {
-			errorNewPasswordTextView.text = getString(R.string.please_enter_your_new_password)
-			errorNewPasswordTextView.visibility = View.VISIBLE
-			DrawableCompat.setTint(newPasswordEditText.background, ContextCompat.getColor(this, R.color.text_error))
-			
-		} else if (newPasswordEditText.text!!.length < 6) {
-			errorNewPasswordTextView.text = getString(R.string.password_must_have_at_least_6_characters)
-			errorNewPasswordTextView.visibility = View.VISIBLE
-			DrawableCompat.setTint(newPasswordEditText.background, ContextCompat.getColor(this, R.color.text_error))
-			
-		}  else if (newPasswordAgainEditText.text.isNullOrEmpty()) {
-			errorNewPasswordAgainTextView.text = getString(R.string.please_confirm_your_new_password)
-			errorNewPasswordAgainTextView.visibility = View.VISIBLE
-			DrawableCompat.setTint(newPasswordAgainEditText.background, ContextCompat.getColor(this, R.color.text_error))
-			
-		}  else if (newPasswordAgainEditText.text!!.length < 6 ) {
-			errorNewPasswordAgainTextView.text = getString(R.string.password_must_have_at_least_6_characters)
-			errorNewPasswordAgainTextView.visibility = View.VISIBLE
-			DrawableCompat.setTint(newPasswordAgainEditText.background, ContextCompat.getColor(this, R.color.text_error))
-			
-		} else if (newPasswordEditText.text.toString() == newPasswordAgainEditText.text.toString()) {
-			passwordChangeViewModel.changeUserPassword(newPasswordEditText.text.toString())
-			sendFeedbackView.hideKeyboard()
-			
-		} else {
-			errorNewPasswordAgainTextView.text = getString(R.string.confirm_password_does_not_match)
-			errorNewPasswordAgainTextView.visibility = View.VISIBLE
-			DrawableCompat.setTint(newPasswordAgainEditText.background, ContextCompat.getColor(this, R.color.text_error))		}
 	}
 	
 	private fun setupToolbar() {
