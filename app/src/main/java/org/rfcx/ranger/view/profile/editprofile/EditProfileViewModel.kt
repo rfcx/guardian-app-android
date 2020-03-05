deb6c6e2f387536f62b8e045645e3e06a1a6751c
ratree <ratree.ochn@gmail.com>
2020-03-05 15:41:36 +0700

    CA-2073 Fix can't upload profile image more than 2000px x 2000px

--- app/src/main/java/org/rfcx/ranger/view/profile/editprofile/EditProfileViewModel.kt
+++ app/src/main/java/org/rfcx/ranger/view/profile/editprofile/EditProfileViewModel.kt
@@ -1,6 +1,8 @@
 package org.rfcx.ranger.view.profile.editprofile
 
 import android.content.Context
+import android.content.ContextWrapper
+import android.graphics.Bitmap
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -15,6 +17,10 @@ import org.rfcx.ranger.entity.ProfilePhotoResponse
 import org.rfcx.ranger.util.getResultError
 import org.rfcx.ranger.util.updateUserProfile
 import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.util.*
 
 class EditProfileViewModel(private val context: Context, private val profilePhotoUseCase: ProfilePhotoUseCase) : ViewModel() {
 	
@@ -25,7 +31,7 @@ class EditProfileViewModel(private val context: Context, private val profilePhot
 		_status.value = Result.Loading
 		
 		val imageFile = File(path)
-		val compressedFile = compressFile(context, imageFile)
+		val compressedFile = checkSizeImage(context, imageFile)
 		
 		profilePhotoUseCase.execute(object : DisposableSingleObserver<ProfilePhotoResponse>() {
 			override fun onSuccess(t: ProfilePhotoResponse) {
@@ -46,11 +52,75 @@ class EditProfileViewModel(private val context: Context, private val profilePhot
 		return MultipartBody.Part.createFormData("file", file.name, requestFile)
 	}
 	
-	private fun compressFile(context: Context?, file: File): File {
+	private fun checkSizeImage(context: Context?, file: File): File {
+		val compressed = Compressor(context)
+				.setQuality(75)
+				.compressToBitmap(file)
+		val excessHeight = compressed.height - 2000
+		val excessWidth = compressed.width - 2000
 		
+		if (compressed.width > 2000 && compressed.height > 2000) {
+			when ((excessWidth).compareTo(excessHeight)) {
+				-1 -> { // less than
+					return bitmapToFile(resizeBitmap(compressed, compressed.width - excessHeight, compressed.height - excessHeight))
+				}
+				0 -> {  // equals
+					return bitmapToFile(resizeBitmap(compressed, compressed.width - excessWidth, compressed.height - excessHeight))
+				}
+				1 -> { // more than
+					return bitmapToFile(resizeBitmap(compressed, compressed.width - excessWidth, compressed.height - excessWidth))
+				}
+			}
+		} else if (compressed.width > 2000) {
+			return bitmapToFile(resizeBitmap(compressed, compressed.width - excessWidth, compressed.height - excessWidth))
+		} else if (compressed.height > 2000) {
+			return bitmapToFile(resizeBitmap(compressed, compressed.width - excessHeight, compressed.height - excessHeight))
+		}
+		
+		val compressedFile = Compressor(context)
+				.setQuality(75)
+				.compressToFile(file)
+		if (compressedFile.length() > 1_000_000) {
+			return compressFile(context, compressedFile)
+		}
+		return compressedFile
+	}
+	
+	// Method to save an bitmap to a file
+	private fun bitmapToFile(bitmap: Bitmap): File {
+		// Get the context wrapper
+		val wrapper = ContextWrapper(context)
+		
+		// Initialize a new file instance to save bitmap object
+		var file = wrapper.getDir("Images", Context.MODE_PRIVATE)
+		file = File(file, "${UUID.randomUUID()}.jpg")
+		
+		try {
+			// Compress the bitmap and save in jpg format
+			val stream: OutputStream = FileOutputStream(file)
+			bitmap.compress(Bitmap.CompressFormat.JPEG, 75, stream)
+			stream.flush()
+			stream.close()
+		} catch (e: IOException) {
+			e.printStackTrace()
+		}
+		
+		if (file.length() > 2_000_000) {
+			return compressFile(context, file)
+		}
+		
+		return file
+	}
+	
+	private fun resizeBitmap(bitmap: Bitmap, width: Int, height: Int): Bitmap {
+		return Bitmap.createScaledBitmap(bitmap, width, height, false)
+	}
+	
+	private fun compressFile(context: Context?, file: File): File {
 		if (file.length() <= 0) {
 			return file
 		}
+		
 		val compressed = Compressor(context)
 				.setQuality(75)
 				.compressToFile(file)
