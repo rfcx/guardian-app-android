84b3be4cd474575e1059c26d12c9d776b970c9b8
ratree <ratree.ochn@gmail.com>
2020-03-19 10:46:47 +0700

    CA-2108  Added API "v1/users/accept-terms"

---
+++ app/src/main/java/org/rfcx/ranger/data/remote/terms/TermsRepository.kt
@@ -0,0 +1,11 @@
+package org.rfcx.ranger.data.remote.terms
+
+import io.reactivex.Single
+import org.rfcx.ranger.entity.terms.TermsRequest
+import org.rfcx.ranger.entity.terms.TermsResponse
+
+interface TermsRepository {
+	
+	fun sendBodyPayload(sendBody: TermsRequest): Single<TermsResponse>
+	
+}
