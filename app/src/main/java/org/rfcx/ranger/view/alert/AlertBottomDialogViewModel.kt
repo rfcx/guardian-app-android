ef05c621ea5c754dde774158f4d2e328b5efbb4e
ratreeochn <ratree.ochn@gmail.com>
2020-12-11 23:07:02 +0700

    PI-133 Set width and height

--- app/src/main/java/org/rfcx/ranger/view/alert/AlertBottomDialogViewModel.kt
+++ app/src/main/java/org/rfcx/ranger/view/alert/AlertBottomDialogViewModel.kt
@@ -116,7 +116,7 @@ class AlertBottomDialogViewModel(private val context: Context,
 	}
 	
 	private fun setSpectrogramImage() {
-		_spectrogramImage.value = "https://assets.rfcx.org/audio/${eventResult?.audioId}.png?width=512&height=512" +
+		_spectrogramImage.value = "https://assets.rfcx.org/audio/${eventResult?.audioId}.png?width=400&height=300" +
 				"&inline=1"
 	}
 	
