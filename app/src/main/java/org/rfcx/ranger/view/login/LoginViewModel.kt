884e8ee5fa3c33aedf202da3c79bdb6150ab29d9
ratreeochn <ratree.ochn@gmail.com>
2020-12-21 16:01:12 +0700

    PI-92 Fix if else

--- app/src/main/java/org/rfcx/ranger/view/login/LoginViewModel.kt
+++ app/src/main/java/org/rfcx/ranger/view/login/LoginViewModel.kt
@@ -186,7 +186,6 @@ class LoginViewModel(private val context: Context, private val checkUserTouchUse
 		checkUserTouchUseCase.execute(object : DisposableSingleObserver<Boolean>() {
 			override fun onSuccess(t: Boolean) {
 				val isConsentGiven = preferenceHelper.getBoolean(Preferences.CONSENT_GIVEN)
-				val guardianGroup = preferenceHelper.getString(Preferences.SELECTED_GUARDIAN_GROUP_FULLNAME)
 				
 				if (userAuthResponse.isRanger) {
 					preferenceHelper.putBoolean(Preferences.IS_RANGER, true)
@@ -194,7 +193,7 @@ class LoginViewModel(private val context: Context, private val checkUserTouchUse
 						_redirectPage.postValue(LoginRedirect.TERMS_AND_SERVICE)
 					} else if (context.getUserNickname().substring(0, 1) == "+") {
 						_redirectPage.postValue(LoginRedirect.SET_USER_NAME)
-					} else if (guardianGroup == null){
+					} else {
 						_redirectPage.postValue(LoginRedirect.SET_PROJECTS)
 					}
 				} else {
