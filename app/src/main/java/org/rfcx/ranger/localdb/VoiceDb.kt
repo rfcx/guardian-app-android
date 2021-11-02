61fc53f7023bc902c95362571c79d322b97779fe
RatreeOchn <ratree.ochn@gmail.com>
2021-11-02 16:10:36 +0700

    CE-1496 add Voice Sync Worker

--- app/src/main/java/org/rfcx/ranger/localdb/VoiceDb.kt
+++ app/src/main/java/org/rfcx/ranger/localdb/VoiceDb.kt
@@ -6,6 +6,53 @@ import org.rfcx.ranger.entity.response.SyncState
 import org.rfcx.ranger.entity.response.Voice
 
 class VoiceDb(val realm: Realm) {
+	fun lockUnsent(): List<Voice> {
+		var unsentCopied: List<Voice> = listOf()
+		realm.executeTransaction { it ->
+			val unsent = it.where(Voice::class.java)
+					.equalTo(Voice.FIELD_SYNC_STATE, SyncState.UNSENT.value)
+					.isNotNull(Voice.FIELD_RESPONSE_SERVER_ID)
+					.findAll().createSnapshot()
+			unsentCopied = unsent.toList()
+			unsent.forEach {
+				it.syncState = SyncState.SENDING.value
+			}
+		}
+		return unsentCopied
+	}
+	
+	fun saveReportServerId(serverId: String, reportId: Int) {
+		val voices = realm.where(Voice::class.java)
+				.equalTo(Voice.FIELD_RESPONSE_ID, reportId)
+				.findAll()
+		realm.executeTransaction { transaction ->
+			voices?.forEach {
+				val voice = it.apply {
+					this.responseServerId = serverId
+				}
+				transaction.insertOrUpdate(voice)
+			}
+		}
+	}
+	
+	fun markSent(id: Int, remotePath: String?) {
+		mark(id = id, syncState = SyncState.SENT.value, remotePath)
+	}
+	
+	fun markUnsent(id: Int) {
+		mark(id = id, syncState = SyncState.UNSENT.value, null)
+	}
+	
+	private fun mark(id: Int, syncState: Int, remotePath: String?) {
+		realm.executeTransaction {
+			val report = it.where(Voice::class.java).equalTo(Voice.FIELD_ID, id).findFirst()
+			if (report != null) {
+				report.syncState = syncState
+				report.remotePath = remotePath
+			}
+		}
+	}
+	
 	fun unsentCount(): Long {
 		return realm.where(Voice::class.java).notEqualTo(Voice.FIELD_SYNC_STATE, SyncState.SENT.value).count()
 	}
---
+++ app/src/main/java/org/rfcx/ranger/service/VoiceSyncWorker.kt
@@ -0,0 +1,68 @@
+package org.rfcx.ranger.service
+
+import android.content.Context
+import android.net.Uri
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.work.*
+import io.realm.Realm
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import org.rfcx.ranger.BuildConfig
+import org.rfcx.ranger.data.remote.service.ServiceFactory
+import org.rfcx.ranger.localdb.VoiceDb
+import org.rfcx.ranger.util.RealmHelper
+import java.io.File
+
+class VoiceSyncWorker(private val context: Context, params: WorkerParameters)
+	: Worker(context, params) {
+	override fun doWork(): Result {
+		val assetsService = ServiceFactory.makeAssetsService(BuildConfig.DEBUG, context)
+		val db = VoiceDb(Realm.getInstance(RealmHelper.migrationConfig()))
+		val voices = db.lockUnsent()
+		var someFailed = false
+		Log.d(TAG, "doWork: found ${voices.size} unsent")
+		
+		for (voice in voices) {
+			voice.responseServerId?.let { serverId ->
+				val audioFileOrNull = if (voice.localPath.isNotEmpty()) createLocalFilePart("file", Uri.parse(voice.localPath), "audio/mpeg") else null
+				audioFileOrNull?.let { audioFile ->
+					val result = assetsService.uploadAssets(serverId, audioFile).execute()
+					if (result.isSuccessful) {
+						val assetPath = result.headers()["Location"]
+						assetPath?.let { path ->
+							db.markSent(voice.id, path.substring(1, path.length))
+						}
+					} else {
+						db.markUnsent(voice.id)
+						someFailed = true
+					}
+				}
+			}
+		}
+		
+		return if (someFailed) Result.retry() else Result.success()
+	}
+	
+	private fun createLocalFilePart(partName: String, fileUri: Uri, mediaType: String): MultipartBody.Part {
+		val file = File(fileUri.path)
+		val requestFile = RequestBody.create(mediaType.toMediaTypeOrNull(), file)
+		return MultipartBody.Part.createFormData(partName, file.name, requestFile)
+	}
+	
+	companion object {
+		private const val TAG = "VoiceSyncWorker"
+		private const val UNIQUE_WORK_KEY = "VoiceSyncWorkerUniqueKey"
+		
+		fun enqueue() {
+			val constraints = Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
+			val workRequest = OneTimeWorkRequestBuilder<VoiceSyncWorker>().setConstraints(constraints).build()
+			WorkManager.getInstance().enqueueUniqueWork(UNIQUE_WORK_KEY, ExistingWorkPolicy.KEEP, workRequest)
+		}
+		
+		fun workInfos(): LiveData<List<WorkInfo>> {
+			return WorkManager.getInstance().getWorkInfosForUniqueWorkLiveData(UNIQUE_WORK_KEY)
+		}
+	}
+}
