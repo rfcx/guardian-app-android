3ed831763f5a25d61b72435efde4f901c8d0ed2b
RatreeOchn <ratree.ochn@gmail.com>
2021-11-23 13:17:58 +0700

    CE-1537 Add New alert detail

---
+++ app/src/main/java/org/rfcx/incidents/view/alert/AlertDetailActivity.kt
@@ -0,0 +1,12 @@
+package org.rfcx.incidents.view.alert
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.rfcx.incidents.R
+
+class AlertDetailActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_alert_detail)
+	}
+}
