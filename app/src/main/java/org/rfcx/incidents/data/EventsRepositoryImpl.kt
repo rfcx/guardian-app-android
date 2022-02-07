7d1861f73703716e7de187bc0a7e8afba5a72c4e
wasuphon <wasuphonn59@email.nu.ac.th>
2022-02-07 15:16:48 +0700

    Show event by stream detail

--- app/src/main/java/org/rfcx/incidents/data/EventsRepositoryImpl.kt
+++ app/src/main/java/org/rfcx/incidents/data/EventsRepositoryImpl.kt
@@ -3,12 +3,14 @@ package org.rfcx.incidents.data
 import io.reactivex.Single
 import org.rfcx.incidents.data.interfaces.EventsRepository
 import org.rfcx.incidents.data.local.EventDb
+import org.rfcx.incidents.data.local.StreamDb
 import org.rfcx.incidents.entity.event.Event
 
 class EventsRepositoryImpl(
-    private val eventDb: EventDb
+    private val streamDb: StreamDb
 ) : EventsRepository {
     override fun get(streamId: String): Single<List<Event>> {
-        return Single.just(eventDb.getEvents(streamId))
+        val stream = streamDb.get(streamId)
+        return Single.just(stream?.lastIncident?.events)
     }
 }
