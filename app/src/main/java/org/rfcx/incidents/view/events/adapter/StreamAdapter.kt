14f28989e352379462ad7263a86cf3c5897a4aa1
RatreeOchn <ratree.ochn@gmail.com>
2022-03-25 13:04:54 +0700

    Fix lint

--- app/src/main/java/org/rfcx/incidents/view/events/adapter/StreamAdapter.kt
+++ app/src/main/java/org/rfcx/incidents/view/events/adapter/StreamAdapter.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import io.realm.RealmList
 import org.rfcx.incidents.R
 import org.rfcx.incidents.databinding.ItemStreamBinding
 import org.rfcx.incidents.entity.event.Event
