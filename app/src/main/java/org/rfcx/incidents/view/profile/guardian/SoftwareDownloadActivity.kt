75f805c6ecc39cb1d209bb7750a1db11a303eecd
tooseriuz <frongs@rfcx.org>
2023-01-27 18:04:28 +0700

    #306 Add loading and react to result from coroutine

--- app/src/main/java/org/rfcx/incidents/view/profile/guardian/SoftwareDownloadActivity.kt
+++ app/src/main/java/org/rfcx/incidents/view/profile/guardian/SoftwareDownloadActivity.kt
@@ -3,14 +3,24 @@ package org.rfcx.incidents.view.profile.guardian
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.rfcx.incidents.data.remote.common.Result
+import org.rfcx.incidents.data.remote.guardian.software.SoftwareResponse
 import org.rfcx.incidents.databinding.ActivitySoftwareDownloadBinding
+import org.rfcx.incidents.view.guardian.checklist.CheckListAdapter
 
-class SoftwareDownloadActivity : AppCompatActivity() {
+class SoftwareDownloadActivity : AppCompatActivity(), SoftwareEventListener {
 
     private lateinit var binding: ActivitySoftwareDownloadBinding
     private val viewModel: SoftwareDownloadViewModel by viewModel()
+    private val softwareAdapter by lazy { SoftwareDownloadAdapter(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +28,34 @@ class SoftwareDownloadActivity : AppCompatActivity() {
         binding = ActivitySoftwareDownloadBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewModel.getSoftwareFromRemote()
         setupToolbar()
         setToolbarTitle("Software Download")
+
+        viewModel.getSoftwareFromRemote()
+
+        binding.softwareRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = softwareAdapter
+        }
+
+        lifecycleScope.launch {
+            viewModel.softwareItemState.collectLatest { result ->
+                when (result) {
+                    is Result.Error -> {
+                        Toast.makeText(this@SoftwareDownloadActivity, result.throwable.message, Toast.LENGTH_LONG).show()
+                    }
+                    Result.Loading -> {
+                        binding.softwareLoading.visibility = View.VISIBLE
+                        binding.softwareRecyclerView.visibility = View.GONE
+                    }
+                    is Result.Success -> {
+                        binding.softwareLoading.visibility = View.GONE
+                        binding.softwareRecyclerView.visibility = View.VISIBLE
+                        softwareAdapter.availableSoftwares = result.data
+                    }
+                }
+            }
+        }
     }
 
     fun setupToolbar() {
@@ -43,5 +78,13 @@ class SoftwareDownloadActivity : AppCompatActivity() {
             val intent = Intent(context, SoftwareDownloadActivity::class.java)
             context.startActivity(intent)
         }
+    }
+
+    override fun onDownloadClicked(software: SoftwareResponse) {
+        // TODO
+    }
+
+    override fun onDeleteClicked(software: SoftwareResponse) {
+        // TODO
     }
 }
--- app/src/main/java/org/rfcx/incidents/view/profile/guardian/SoftwareDownloadViewModel.kt
+++ app/src/main/java/org/rfcx/incidents/view/profile/guardian/SoftwareDownloadViewModel.kt
@@ -1,19 +1,25 @@
 package org.rfcx.incidents.view.profile.guardian
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import org.rfcx.incidents.data.remote.common.Result
+import org.rfcx.incidents.data.remote.guardian.software.SoftwareResponse
 import org.rfcx.incidents.domain.guardian.software.GetSoftwareUseCase
 
 class SoftwareDownloadViewModel(private val getSoftwareUseCase: GetSoftwareUseCase) : ViewModel() {
 
+    private val _softwareItemState: MutableStateFlow<Result<List<SoftwareResponse>>> = MutableStateFlow(Result.Loading)
+    val softwareItemState = _softwareItemState.asStateFlow()
+
     fun getSoftwareFromRemote() {
         viewModelScope.launch(Dispatchers.IO) {
-            getSoftwareUseCase.launch().collect {
-                Log.d("Comp", it.toString())
+            getSoftwareUseCase.launch().collect { result ->
+                _softwareItemState.tryEmit(result)
             }
         }
     }
