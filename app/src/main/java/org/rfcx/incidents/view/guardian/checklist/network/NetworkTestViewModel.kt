71a29b454f562fc903247751302e26b4b5ee714a
TooSeriuz <frongs@rfcx.org>
2023-03-15 17:21:30 +0700

    Show sat signal badge

--- app/src/main/java/org/rfcx/incidents/view/guardian/checklist/network/NetworkTestViewModel.kt
+++ app/src/main/java/org/rfcx/incidents/view/guardian/checklist/network/NetworkTestViewModel.kt
@@ -13,6 +13,7 @@ import org.rfcx.incidents.util.socket.PingUtils.getSimDetected
 import org.rfcx.incidents.util.socket.PingUtils.getSimNetwork
 import org.rfcx.incidents.util.socket.PingUtils.getSpeedTest
 import org.rfcx.incidents.util.socket.PingUtils.getSwarmId
+import org.rfcx.incidents.util.socket.PingUtils.getSwarmNetwork
 
 class NetworkTestViewModel(
     private val getGuardianMessageUseCase: GetGuardianMessageUseCase,
@@ -28,8 +29,38 @@ class NetworkTestViewModel(
     private val _simSignalState: MutableStateFlow<Int?> = MutableStateFlow(null)
     val simSignalState = _simSignalState.asStateFlow()
 
+    private val _simStrength1State: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val simStrength1State = _simStrength1State.asStateFlow()
+
+    private val _simStrength2State: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val simStrength2State = _simStrength2State.asStateFlow()
+
+    private val _simStrength3State: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val simStrength3State = _simStrength3State.asStateFlow()
+
+    private val _simStrength4State: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val simStrength4State = _simStrength4State.asStateFlow()
+
     private val _internetConnectionState: MutableStateFlow<Boolean> = MutableStateFlow(false)
     val internetConnectionState = _internetConnectionState.asStateFlow()
+
+    private val _satSignalState: MutableStateFlow<Int?> = MutableStateFlow(null)
+    val satSignalState = _satSignalState.asStateFlow()
+
+    private val _satErrorState: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val satErrorState = _satErrorState.asStateFlow()
+
+    private val _satBadState: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val satBadState = _satBadState.asStateFlow()
+
+    private val _satOKState: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val satOKState = _satOKState.asStateFlow()
+
+    private val _satGoodState: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val satGoodState = _satGoodState.asStateFlow()
+
+    private val _satPerfectState: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val satPerfectState = _satPerfectState.asStateFlow()
 
     init {
         getSimModule()
@@ -46,6 +77,7 @@ class NetworkTestViewModel(
                 }
                 result?.getSimNetwork()?.let {
                     _simSignalState.tryEmit(it)
+                    setSimStrengthState(it)
                 }
                 result?.getSpeedTest()?.let {
                     _internetConnectionState.tryEmit(it.hasConnection)
@@ -62,6 +94,91 @@ class NetworkTestViewModel(
                 result?.getSwarmId()?.let {
                     _satModuleState.tryEmit(it.isNotEmpty())
                 }
+            }
+        }
+        viewModelScope.launch {
+            getGuardianMessageUseCase.launch().catch {
+
+            }.collectLatest { result ->
+                result?.getSwarmNetwork()?.let {
+                    _satSignalState.tryEmit(it)
+                    setSatStrengthState(it)
+                }
+            }
+        }
+    }
+
+    private fun setSimStrengthState(signal: Int) {
+        when {
+            signal < -130 -> {
+                _simStrength1State.tryEmit(false)
+                _simStrength2State.tryEmit(false)
+                _simStrength3State.tryEmit(false)
+                _simStrength4State.tryEmit(false)
+            }
+            signal < -110 -> {
+                _simStrength1State.tryEmit(true)
+                _simStrength2State.tryEmit(false)
+                _simStrength3State.tryEmit(false)
+                _simStrength4State.tryEmit(false)
+            }
+            signal < -90 -> {
+                _simStrength1State.tryEmit(true)
+                _simStrength2State.tryEmit(true)
+                _simStrength3State.tryEmit(false)
+                _simStrength4State.tryEmit(false)
+            }
+            signal < -70 -> {
+                _simStrength1State.tryEmit(true)
+                _simStrength2State.tryEmit(true)
+                _simStrength3State.tryEmit(true)
+                _simStrength4State.tryEmit(false)
+            }
+            else -> {
+                _simStrength1State.tryEmit(true)
+                _simStrength2State.tryEmit(true)
+                _simStrength3State.tryEmit(true)
+                _simStrength4State.tryEmit(true)
+            }
+        }
+    }
+
+    private fun setSatStrengthState(signal: Int) {
+        when {
+            signal <= -110 -> {
+                _satErrorState.tryEmit(true)
+                _satBadState.tryEmit(false)
+                _satOKState.tryEmit(false)
+                _satGoodState.tryEmit(false)
+                _satPerfectState.tryEmit(false)
+            }
+            signal <= -104 -> {
+                _satErrorState.tryEmit(false)
+                _satBadState.tryEmit(false)
+                _satOKState.tryEmit(false)
+                _satGoodState.tryEmit(false)
+                _satPerfectState.tryEmit(true)
+            }
+            signal < -100 -> {
+                _satErrorState.tryEmit(false)
+                _satBadState.tryEmit(false)
+                _satOKState.tryEmit(false)
+                _satGoodState.tryEmit(true)
+                _satPerfectState.tryEmit(false)
+            }
+            signal < -97 -> {
+                _satErrorState.tryEmit(false)
+                _satBadState.tryEmit(false)
+                _satOKState.tryEmit(true)
+                _satGoodState.tryEmit(false)
+                _satPerfectState.tryEmit(false)
+            }
+            signal < -93 -> {
+                _satErrorState.tryEmit(false)
+                _satBadState.tryEmit(true)
+                _satOKState.tryEmit(false)
+                _satGoodState.tryEmit(false)
+                _satPerfectState.tryEmit(false)
             }
         }
     }
