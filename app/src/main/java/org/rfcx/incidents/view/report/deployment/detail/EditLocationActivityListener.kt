508ff2bf727235fb22ee965e254882ca6af1f8cc
TooSeriuz <frongs@rfcx.org>
2023-06-05 18:53:26 +0700

    Add data binding

--- app/src/main/java/org/rfcx/incidents/view/report/deployment/detail/EditLocationActivityListener.kt
+++ app/src/main/java/org/rfcx/incidents/view/report/deployment/detail/EditLocationActivityListener.kt
@@ -2,6 +2,8 @@ package org.rfcx.incidents.view.report.deployment.detail
 
 import org.rfcx.companion.entity.Project
 import org.rfcx.companion.entity.Stream
+import org.rfcx.incidents.entity.stream.Project
+import org.rfcx.incidents.entity.stream.Stream
 
 interface EditLocationActivityListener {
     fun startMapPickerPage(latitude: Double, longitude: Double, altitude: Double, streamId: Int)
