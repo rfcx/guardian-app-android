9f2dd2f4a909909c6ebb05d1008e6b34b83ba627
TooSeriuz <frongs@rfcx.org>
2023-08-02 21:33:40 +0700

    Remove unused old gallery picker

--- app/src/main/java/org/rfcx/incidents/view/report/create/image/BaseImageFragment.kt
+++ app/src/main/java/org/rfcx/incidents/view/report/create/image/BaseImageFragment.kt
@@ -12,6 +12,7 @@ import org.rfcx.incidents.databinding.ButtomSheetAttachImageLayoutBinding
 import org.rfcx.incidents.util.CameraPermissions
 import org.rfcx.incidents.util.GalleryPermissions
 import org.rfcx.incidents.util.ImageFileUtils
+import org.rfcx.incidents.util.ImageUtils
 import org.rfcx.incidents.util.ReportUtils
 import org.rfcx.incidents.view.base.BaseFragment
 import java.io.File
@@ -27,22 +28,19 @@ abstract class BaseImageFragment : BaseFragment() {
     protected val reportImageAdapter by lazy { ReportImageAdapter() }
     private var imageFile: File? = null
 
-    private lateinit var attachImageDialog: BottomSheetDialog
     private val cameraPermissions by lazy { CameraPermissions(requireActivity()) }
     private val galleryPermissions by lazy { GalleryPermissions(requireActivity()) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setupAttachImageDialog()
         setupReportImages()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        handleTakePhotoResult(requestCode, resultCode)
-        handleGalleryResult(requestCode, resultCode, data)
+        handleChooseImage(requestCode, resultCode, data)
     }
 
     private fun setupReportImages() {
@@ -59,98 +57,28 @@ abstract class BaseImageFragment : BaseFragment() {
 
             override fun onDeleteImageClick(position: Int, imagePath: String) {
                 reportImageAdapter.removeAt(position)
-                dismissImagePickerOptionsDialog()
                 didRemoveImage(imagePath)
             }
         }
 
         reportImageAdapter.setImages(arrayListOf())
-        dismissImagePickerOptionsDialog()
-    }
-
-    private fun setupAttachImageDialog() {
-        val binding = ButtomSheetAttachImageLayoutBinding.inflate(LayoutInflater.from(context), null, false)
-        binding.menuGallery.setOnClickListener {
-            openGallery()
-        }
-
-        binding.menuTakePhoto.setOnClickListener {
-            takePhoto()
-        }
-        attachImageDialog = BottomSheetDialog(requireContext())
-        attachImageDialog.setContentView(binding.root)
-    }
-
-    private fun dismissImagePickerOptionsDialog() {
-        attachImageDialog.dismiss()
-    }
-
-    private fun takePhoto() {
-        if (!cameraPermissions.allowed()) {
-            imageFile = null
-            cameraPermissions.check { }
-        } else {
-            startTakePhoto()
-        }
-    }
-
-    private fun startTakePhoto() {
-        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        imageFile = ReportUtils.createReportImageFile()
-
-        val image = imageFile ?: return
-        val photoURI = FileProvider.getUriForFile(requireContext(), ReportUtils.FILE_CONTENT_PROVIDER, image)
-        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-        startActivityForResult(takePictureIntent, ReportUtils.REQUEST_TAKE_PHOTO)
-    }
-
-    private fun handleTakePhotoResult(requestCode: Int, resultCode: Int) {
-        if (requestCode != ReportUtils.REQUEST_TAKE_PHOTO) return
-
-        if (resultCode == Activity.RESULT_OK) {
-            imageFile?.let {
-                val pathList = listOf(it.absolutePath)
-                reportImageAdapter.addImages(pathList)
-                didAddImages(pathList)
-            }
-            dismissImagePickerOptionsDialog()
-        } else {
-            // remove file image
-            imageFile?.let {
-                ImageFileUtils.removeFile(it)
-                this.imageFile = null
-            }
-        }
-    }
-
-    private fun openGallery() {
-        if (!galleryPermissions.allowed()) {
-            imageFile = null
-            galleryPermissions.check { }
-        } else {
-            startOpenGallery()
-        }
     }
 
     private fun startOpenGallery() {
-        val remainingImage = ReportImageAdapter.MAX_IMAGE_SIZE - reportImageAdapter.getImageCount()
-        GligarPicker()
-            .requestCode(ReportUtils.REQUEST_GALLERY)
-            .limit(remainingImage)
-            .withFragment(this)
-            .show()
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+            .setType("image/*")
+            .addCategory(Intent.CATEGORY_OPENABLE)
+        startActivityForResult(intent, ImageUtils.REQUEST_GALLERY)
     }
 
-    private fun handleGalleryResult(requestCode: Int, resultCode: Int, intentData: Intent?) {
-        if (requestCode != ReportUtils.REQUEST_GALLERY || resultCode != Activity.RESULT_OK || intentData == null) return
+    private fun handleChooseImage(requestCode: Int, resultCode: Int, intentData: Intent?) {
+        if (requestCode != ImageUtils.REQUEST_GALLERY || resultCode != Activity.RESULT_OK || intentData == null) return
 
-        val pathList = mutableListOf<String>()
-        val results = intentData.extras?.getStringArray(GligarPicker.IMAGES_RESULT)
-        results?.forEach {
-            pathList.add(it)
+        intentData.data?.also {
+            val path = ImageUtils.createImageFile(it, requireContext())
+            if (path != null) {
+                reportImageAdapter.addImages(listOf(path))
+            }
         }
-        reportImageAdapter.addImages(pathList)
-        didAddImages(pathList)
-        dismissImagePickerOptionsDialog()
     }
 }
