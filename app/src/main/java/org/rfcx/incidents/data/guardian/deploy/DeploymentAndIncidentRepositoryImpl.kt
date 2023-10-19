c648124fd99b684d8c36d103f11444d364a7e3ac
TooSeriuz <frongs@rfcx.org>
2023-10-19 18:39:34 +0700

    Add async await to loading alert and deployment data

--- app/src/main/java/org/rfcx/incidents/data/guardian/deploy/DeploymentAndIncidentRepositoryImpl.kt
+++ app/src/main/java/org/rfcx/incidents/data/guardian/deploy/DeploymentAndIncidentRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package org.rfcx.incidents.data.guardian.deploy
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
@@ -43,36 +45,43 @@ class DeploymentAndIncidentRepositoryImpl(
             currentRunning = cacheKey(projectId)
 
             streamLocal.deleteByProject(projectId)
-            val rawStreamsWithDeployment = deploymentEndpoint.getStreams(projects = listOf(projectId), offset = offset)
-            val rawStreamsWithIncident = incidentEndpoint.getStreamsSuspend(projects = listOf(projectId), offset = offset)
-            var order = offset
-            // save stream and deployment
-            rawStreamsWithDeployment.forEachIndexed { index, rawDP ->
-                var deployment: Deployment? = null
-                rawDP.deployment?.let {
-                    deployment = deploymentLocal.insertWithResult(it.toDeployment())
+            coroutineScope {
+                var order = offset
+                val rawStreamsWithDeploymentDeferred = async { deploymentEndpoint.getStreams(projects = listOf(projectId), offset = offset) }
+                val rawStreamsWithIncidentDeferred = async { incidentEndpoint.getStreamsSuspend(projects = listOf(projectId), offset = offset) }
+
+                val rawStreamsWithDeployment = rawStreamsWithDeploymentDeferred.await()
+                val rawStreamsWithIncident = rawStreamsWithIncidentDeferred.await()
+
+                // save stream and deployment
+                rawStreamsWithDeployment.forEachIndexed { index, rawDP ->
+                    var deployment: Deployment? = null
+                    rawDP.deployment?.let {
+                        deployment = deploymentLocal.insertWithResult(it.toDeployment())
+                    }
+
+                    rawDP.toStream().apply {
+                        this.deployment = deployment
+                        order += index
+                        this.order = order
+                        streamLocal.insertOrUpdate(this)
+                    }
+                }
+                // save stream and incident
+                rawStreamsWithIncident.forEachIndexed { index, rawICD ->
+                    val stream = rawICD.toStream()
+                    order += index
+                    stream.order = order
+                    streamLocal.insertOrUpdate(stream)
+                    rawICD.lastIncident()?.events?.forEach { event ->
+                        eventLocal.insertOrUpdate(event.toEvent(rawICD.id), rawICD.lastIncident()!!.id)
+                    }
                 }
 
-                rawDP.toStream().apply {
-                    this.deployment = deployment
-                    order += index
-                    this.order = order
-                    streamLocal.insertOrUpdate(this)
-                }
+                cachedEndpointDb.updateCachedEndpoint(cacheKey(projectId))
+                currentRunning = ""
+                emit(Result.Success(getLocal(projectId)))
             }
-            // save stream and incident
-            rawStreamsWithIncident.forEachIndexed { index, rawICD ->
-                val stream = rawICD.toStream()
-                order += index
-                stream.order = order
-                streamLocal.insertOrUpdate(stream)
-                rawICD.lastIncident()?.events?.forEach { event ->
-                    eventLocal.insertOrUpdate(event.toEvent(rawICD.id), rawICD.lastIncident()!!.id)
-                }
-            }
-            cachedEndpointDb.updateCachedEndpoint(cacheKey(projectId))
-            currentRunning = ""
-            emit(Result.Success(getLocal(projectId)))
         }.catch {
             emit(Result.Error(it))
             cachedEndpointDb.updateCachedEndpoint(cacheKey(projectId))
