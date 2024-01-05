998a5355544941345ab33a60cf7b8995238a6b93
TooSeriuz <frongs@rfcx.org>
2024-01-05 16:10:42 +0700

    remove change password and change image profile

--- app/src/main/java/org/rfcx/incidents/data/interfaces/ProfilePhotoRepository.kt
+++
@@ -1,9 +0,0 @@
-package org.rfcx.incidents.data.interfaces
-
-import io.reactivex.Single
-import okhttp3.MultipartBody
-import org.rfcx.incidents.entity.user.ProfilePhotoResponse
-
-interface ProfilePhotoRepository {
-    fun sendProfilePhoto(sendBody: MultipartBody.Part): Single<ProfilePhotoResponse>
-}
